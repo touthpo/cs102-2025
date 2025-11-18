1cbc426e2f98a46519ce0b1a2665446f9ff1e51e
touthpo <er_toma@mail.ru>
2025-11-18 12:22:21 +0300

    Доп задача

--- homework01/transposition.py
+++ homework01/transposition.py
@@ -9,13 +9,8 @@ def decrypt_transposition(ciphertext, block_size, id1, id2):
         text += "X" * (block_size - remainder)
     decrypted_text = ""
     for i in range(0, len(text), block_size):
-        block = text[i:i + block_size]
+        block = text[i : i + block_size]
         block_chars = list(block)
         block_chars[id1], block_chars[id2] = block_chars[id2], block_chars[id1]
         decrypted_text += "".join(block_chars)
     return decrypted_text
-
-    
-     
-        
-    
