9be49520363f74cdc6e9ba8b14c73ca650d4169d
touthpo <er_toma@mail.ru>
2026-01-15 01:45:26 +0300

    Исправление ошибок

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -148,7 +148,7 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     return None
 
 
-def check_solution(solution: tp.List[tp.List[str]]) -> bool:
+def check_solution(grip: tp.List[tp.List[str]]) -> bool:
     """Если решение solution верно, то вернуть True, в противном случае False"""
     # TODO: Add doctests with bad puzzles
     for row in range(len(grid)):
