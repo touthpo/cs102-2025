c3be5e2862c17d9dbb37f427787b37780562b3ef
touthpo <er_toma@mail.ru>
2026-01-17 03:59:42 +0300

    life_proto.py

--- homework04/life_proto.py
+++ homework04/life_proto.py
@@ -115,24 +115,6 @@ class GameOfLife:
 
         pygame.quit()
 
-<<<<<<< HEAD
-=======
-        def some_function():
-            """
-            Returns
-            -------
-            out : Cells
-                Список соседних клеток, в котором каждая позиция - 0 или 1.
-            """
-            pass
-
-        Returns
-        ----------
-        out : Cells
-            Список соседних клеток, в котором каждая позиция – 0 или 1.
-        """
-        pass
->>>>>>> origin/homework04
 
 if __name__ == "__main__":
     game = GameOfLife(320, 240, 20)
